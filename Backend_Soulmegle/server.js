b8ceaf273c8c64a3438ca4f704a86e38ed26c682
Ayush Badola <glitchy.56@outlook.com>
2025-05-31 21:42:14 +0530

    Tweaking things

--- Backend_Soulmegle/server.js
+++ Backend_Soulmegle/server.js
@@ -21,9 +21,6 @@ app.post('/api/mood', (req, res) => {
   res.json({ message });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../Frontend_Soulmegle/dist/index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
